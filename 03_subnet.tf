881ec4ae7ffa1eb8698dbdeb8530e2e05f51a411
hhhhjjjjjjiiii <wnlwnl0522@gmail.com>
2021-11-11 00:52:48 +0900

    'step1'

---
+++ 03_subnet.tf
@@ -0,0 +1,36 @@
+# 가용영역 a의 public subnet
+resource "aws_subnet" "hjkim_puba" {
+  vpc_id     = aws_vpc.hjkim_vpc.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "ap-northeast-2a"
+  tags = {
+    Name = "hjkim-puba"
+  }
+}
+# 가용영역 a의 private subnet
+resource "aws_subnet" "hjkim_pria" {
+  vpc_id     = aws_vpc.hjkim_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-northeast-2a"
+  tags = {
+    Name = "hjkim-pria"
+  }
+}
+# 가용영역 c의 public subnet
+resource "aws_subnet" "hjkim_pubc" {
+  vpc_id     = aws_vpc.hjkim_vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "ap-northeast-2c"
+  tags = {
+    Name = "hjkim-pubc"
+  }
+}
+# 가용영역 c의 private subnet
+resource "aws_subnet" "hjkim_pric" {
+  vpc_id     = aws_vpc.hjkim_vpc.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "ap-northeast-2c"
+  tags = {
+    Name = "hjkim-pric"
+  }
+}
