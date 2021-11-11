181a4472e243e55146e718e636c5b008d0402bf2
hhhhjjjjjjiiii <wnlwnl0522@gmail.com>
2021-11-11 17:07:09 +0900

    'step2'

--- 09_ngass.tf
+++ 09_ngass.tf
@@ -1,9 +1,9 @@
 resource "aws_route_table_association" "hjkim_ngass_a" {
-  subnet_id = aws_subnet.hjkim_pria.id
-  route_table_id = aws_route_table.hjkim_ngrt.id  
+  subnet_id      = aws_subnet.hjkim_pria.id
+  route_table_id = aws_route_table.hjkim_ngrt.id
 }
 
 resource "aws_route_table_association" "hjkim_ngass_c" {
-  subnet_id = aws_subnet.hjkim_pric.id
-  route_table_id = aws_route_table.hjkim_ngrt.id  
+  subnet_id      = aws_subnet.hjkim_pric.id
+  route_table_id = aws_route_table.hjkim_ngrt.id
 }
