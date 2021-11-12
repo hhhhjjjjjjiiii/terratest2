401197c764f9736e24ce8e17aad9a6ce7fff5bfb
hhhhjjjjjjiiii <wnlwnl0522@gmail.com>
2021-11-12 12:26:46 +0900

    'step3'

--- 11_ec2.tf
+++ 11_ec2.tf
@@ -17,14 +17,14 @@ data "aws_ami" "amzn" {
 */
 
 resource "aws_instance" "hjkim_weba" {
-  ami = "ami-04e8dfc09b22389ad"
-  instance_type = "t2.micro"
-  key_name = "hjkim-key"
-  availability_zone = "ap-northeast-2a"
-  private_ip = "10.0.0.11"
-  subnet_id = aws_subnet.hjkim_puba.id
+  ami                    = "ami-04e8dfc09b22389ad"
+  instance_type          = "t2.micro"
+  key_name               = "hjkim-key"
+  availability_zone      = "ap-northeast-2a"
+  private_ip             = "10.0.0.11"
+  subnet_id              = aws_subnet.hjkim_puba.id
   vpc_security_group_ids = [aws_security_group.hjkim_sg.id]
-  user_data =  <<-EOF
+  user_data              = <<-EOF
                  #!/bin/bash
                  yum install -y httpd
                  echo "HJKIM-Terraform-1" >> /var/www/html/index.html
@@ -33,14 +33,14 @@ resource "aws_instance" "hjkim_weba" {
 }
 
 resource "aws_eip" "hjkim_weba_ip" {
-  vpc = true
-  instance = aws_instance.hjkim_weba.id
+  vpc                       = true
+  instance                  = aws_instance.hjkim_weba.id
   associate_with_private_ip = "10.0.0.11"
   depends_on = [
     aws_internet_gateway.hjkim_ig
   ]
-  }
+}
 
-   output "public_ip" {
-     value = aws_instance.hjkim_weba.public_ip
-   }
+output "public_ip" {
+  value = aws_instance.hjkim_weba.public_ip
+}
