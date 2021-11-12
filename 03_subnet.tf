422253acca2e024c413dbe80e46b23bc82b21681
hhhhjjjjjjiiii <wnlwnl0522@gmail.com>
2021-11-12 16:30:26 +0900

    'step2'

--- 03_subnet.tf
+++ 03_subnet.tf
@@ -34,3 +34,21 @@ resource "aws_subnet" "hjkim_pric" {
     Name = "hjkim-pric"
   }
 }
+# 가용영역 a의 private DB subnet
+resource "aws_subnet" "hjkim_pridba" {
+  vpc_id     = aws_vpc.hjkim_vpc.id
+  cidr_block = "10.0.4.0/24"
+  availability_zone = "ap-northeast-2a"
+  tags = {
+    Name = "hjkim-pridba"
+  }
+}
+# 가용영역 c의 private DB subnet
+resource "aws_subnet" "hjkim_pridbc" {
+  vpc_id     = aws_vpc.hjkim_vpc.id
+  cidr_block = "10.0.5.0/24"
+  availability_zone = "ap-northeast-2c"
+  tags = {
+    Name = "hjkim-pridbc"
+  }
+}
