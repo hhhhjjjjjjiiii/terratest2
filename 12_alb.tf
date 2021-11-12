5d34f46f9b6a3b46dde091154d369fd1ab19d8a1
hhhhjjjjjjiiii <wnlwnl0522@gmail.com>
2021-11-12 12:27:19 +0900

    'stpe2'

--- 12_alb.tf
+++ 12_alb.tf
@@ -1,9 +1,9 @@
 resource "aws_lb" "hjkim_alb" {
-  name = "hjkim-alb"
-  internal = false
+  name               = "hjkim-alb"
+  internal           = false
   load_balancer_type = "application"
-  security_groups = [aws_security_group.hjkim_sg.id]
-  subnets = [aws_subnet.hjkim_puba.id, aws_subnet.hjkim_pubc.id]
+  security_groups    = [aws_security_group.hjkim_sg.id]
+  subnets            = [aws_subnet.hjkim_puba.id, aws_subnet.hjkim_pubc.id]
 
   tags = {
     "Name" = "hjkim-alb"
